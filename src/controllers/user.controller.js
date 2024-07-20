badb39c43e29e662d41041457e383dd15ada574b
harmistervadiya <154576954+HarmishTervadiya@users.noreply.github.com>
2024-07-20 19:38:52 +0530

    fix register api  all bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,7 +14,7 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -23,14 +23,19 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
+
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    
 
     if (!avatar) {
         throw new ApiError(400, "Avatar did not uploaded")
@@ -54,7 +59,7 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(500, "Something went wrong while registering the user")
     }
 
-    return new res.status(201).json(
+    return res.status(201).json(
         new ApiResponse(200, createdUser, "User registered successfully")
     )
     
